4f74c7e56f86e00daa1a0dcff32ff861e0a723a9
Vadym <100239066+Wertsaq@users.noreply.github.com>
2024-09-26 10:30:38 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,10 +1,16 @@
 <?php
 session_start();
 
+$DB_HOST = getenv('DB_HOST');
+$DB_USER = getenv('DB_USER');
+$DB_PASSWORD = getenv('DB_PASSWORD');
+$DB_NAME = getenv('DB_NAME');
+
 $conn = mysqli_connect(
-  getenv('DB_HOST'),
-  getenv('DB_USER'),
-  getenv('DB_PASSWORD'),
-  getenv('DB_NAME')
+  $DB_HOST,
+  $DB_USER,
+  $DB_PASSWORD,
+  $DB_NAME
 ) or die(mysqli_error($mysqli));
+
 ?>
